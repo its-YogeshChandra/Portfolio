4c93bf49d7cf536f92f8a759af0dc3871a634270
its-YogeshChandra <pandittheroyal@gmail.com>
2026-02-04 23:27:45 +0530

    Add social media icon and profile image assets, then update the Hero section to display them using Next.js Image component.

--- src/components/hero.section.tsx
+++ src/components/hero.section.tsx
@@ -1,21 +1,29 @@
 import { Badge } from "@/components/ui/badge";
 import React from "react";
 import { Mail, Github, Linkedin, } from 'lucide-react';
+import twitter from "../../public/twitter.png"
+import github from "../../public/github.png"
+import linkedin from "../../public/linkedin.png"
+import mail from "../../public/email.png"
+import profile from "../../public/wellington-ferreira-72TE8cWKXRY-unsplash.jpg"
 
+import Image from "next/image";
 
 export const HeroSection = () => {
+
+    //have to provide links to the buttons 
     return (
         <div className="w-full h-max mt-10 flex flex-col gap-y-5">
             <Badge className="px-6 py-2 text-black bg-gray-100 flex gap-x-3 ">
                 <div className="w-3 h-3 rounded-full bg-green-500"></div>
                 <p className="text-[18px]">Available for work</p>
             </Badge>
-            <div className="w-max h-max flex gap-x-3 items-center">
+            <div className="w-max h-max flex gap-x-3 items-center mt-4">
                 <div className="w-20 h-[3px] bg-linear-to-r from-gray-600 to-gray-200"></div>
                 <p className="text-3xl font-bold"> ABOUT/PROFILE</p>
             </div>
-            <div className="w-full min-h-100  flex gap-x-2 mt-6">
-                <div className="w-2/3 min-h-100  flex flex-col">
+            <div className="w-full min-h-100  flex gap-x-2 ">
+                <div className="w-2/3 min-h-100  flex flex-col mt-15">
                     <div>
                         <p className="text-4xl font-extrabold">Full Stack </p>
                         <p className="text-4xl font-extrabold">Developer</p>
@@ -23,20 +31,38 @@ export const HeroSection = () => {
                     <div className="w-full h-20 border-l-4 border-black mt-4 px-2">
                         <p className="text-[18px]">Full-stack developer with a strong interest in blockchain and Rust. I enjoy building scalable systems, clean APIs, and reliable backends with modern web technologies</p>
                     </div>
-                    <div className="mt-4 w-full h-20 flex gap-x-4 items-center bg-green-400">
-                        <Badge className=" px-3 py-2 flex"> <Github size={24} /> 
-                        <p>GitHub</p>
+                    <div className="mt-4 w-full h-20 flex gap-x-6 items-center ">
+                       <button>
+                        <Badge className=" px-3 py-2 flex bg-gray-100 text-black" >
+                            <Image src={github} alt="github" className="w-4 h-4" />
+                        <p className="text-sm">GitHub</p>
                         </Badge>
-                        <Badge className=" px-3 py-2 flex"> <Linkedin size={24} /> 
-                        <p>Linkedin</p>
+                       </button>
+                       <button>
+                        <Badge className=" px-3 py-2 flex bg-gray-100 text-black"> 
+                            <Image src={linkedin} alt="linkedin" className="w-4 h-4" />
+                        <p className="text-sm">Linkedin</p>
                         </Badge>
-                        <Badge className=" px-3 py-2 flex"> <Mail size={24} /> 
-                        <p>Mail</p>
+                       </button>
+                       <button>
+                        <Badge className=" px-3 py-2 flex bg-gray-100 text-black"> 
+                            <Image src={mail} alt="mail" className="w-4 h-4" />
+                        <p className="text-sm">Mail</p>
                         </Badge>
-                        <Badge className=" px-3 py-2"></Badge>
+                       </button>
+                       <button>
+                        <Badge className=" px-3 py-2 flex bg-gray-100 text-black">
+                            <Image src={twitter} alt="twitter" className="w-4 h-4" />
+                            <p className="text-sm">Twitter</p>
+                        </Badge>
+                       </button>
                     </div>
                 </div>
-                <div className="w-1/2 min-h-100 bg-blue-500"></div>
+                <div className="w-1/2 min-h-100 flex pl-5">
+                 <div className="w-72 h-72 rounded-full bg-red-500">
+                    <Image src={profile} alt="profile" className="w-full h-full rounded-full border-2 border-white" />
+                 </div>
+                </div>
             </div>
         </div>
     )
